6ee8e3ce252ead40edf687dd1d357f6726c9167d
Mason <maseh87@gmail.com>
2014-06-13 16:04:01 -0700

    created blue enemies

--- watchout.js
+++ watchout.js
@@ -1,3 +1,38 @@
+<<<<<<< HEAD
+// make the game board settings
+
+var gameSettings = {
+  height: 450,
+  width: 700,
+  nEnemies: 30,
+  padding: 20
+}
+
+//make the Enemies
+var makeEnemies = function () {
+  return _.range(0, gameSettings.nEnemies).map(function(i) {
+    return {
+      id: i,
+      x: Math.random() * 800,
+      y: Math.random() * 600
+    }
+  })
+};
+
+var enemy = makeEnemies();
+
+
+//make the board
+var board = d3.select('body').append('svg').attr({width: gameSettings.width, height: gameSettings.height, class: "gameBoard"});
+
+
+//renderEnemies on the board
+var renderEnemies = board.selectAll('circle').data(enemy).enter().append('circle').attr('cx', function(i){ return i.x; }).attr('cy', function(i){ return i.y; }).attr('id', function(i){ return i.id; }).attr('r', 10).attr('fill', 'blue' );
+
+
+
+// board;
+=======
 // start slingin' some d3 here.
 //add the svg element representing our playing Field
 var svg = d3.select('body').append('svg').attr('width', 700).attr('height', 450);
@@ -27,3 +62,4 @@ var makePlayer = function() {
 
 //Puts the player and enemies on the board
 d3.select('svg').append('g').attr('');
+>>>>>>> ff4ace821a7dd6b6e42e3a4abdab5fb12a07e7b1
