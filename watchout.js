cb27f9ac20035aa9210f2ae66568975547359792
Mason <maseh87@gmail.com>
2014-06-13 16:06:17 -0700

    updates

--- watchout.js
+++ watchout.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // make the game board settings
 
 var gameSettings = {
@@ -28,38 +27,3 @@ var board = d3.select('body').append('svg').attr({width: gameSettings.width, hei
 
 //renderEnemies on the board
 var renderEnemies = board.selectAll('circle').data(enemy).enter().append('circle').attr('cx', function(i){ return i.x; }).attr('cy', function(i){ return i.y; }).attr('id', function(i){ return i.id; }).attr('r', 10).attr('fill', 'blue' );
-
-
-
-// board;
-=======
-// start slingin' some d3 here.
-//add the svg element representing our playing Field
-var svg = d3.select('body').append('svg').attr('width', 700).attr('height', 450);
-
-
-
-
-//Create the enemies
-var makeEnemies = function() {
-
-
-
-};
-
-//Create the player
-var makePlayer = function() {
-  //return an object with the id, the x and y axis'
-  return [{
-    x: 300,
-    y: 400,
-    id: 'player'
-  }];
-};
-
-
-
-
-//Puts the player and enemies on the board
-d3.select('svg').append('g').attr('');
->>>>>>> ff4ace821a7dd6b6e42e3a4abdab5fb12a07e7b1
