c98fbfc4faa186f09f97c8d96bf550a238e565d3
Hack Reactor Students <maseh87@gmail.com>
2014-06-13 15:33:44 -0700

    setting up

--- watchout.js
+++ watchout.js
@@ -1,2 +1,29 @@
 // start slingin' some d3 here.
+//add the svg element representing our playing Field
+var svg = d3.select('body').append('svg').attr('width', 700).attr('height', 450);
 
+
+
+
+//Create the enemies
+var makeEnemies = function() {
+
+
+
+};
+
+//Create the player
+var makePlayer = function() {
+  //return an object with the id, the x and y axis'
+  return [{
+    x: 300,
+    y: 400,
+    id: 'player'
+  }];
+};
+
+
+
+
+//Puts the player and enemies on the board
+d3.select('svg').append('g').attr('');
