68b27120e026c2a0bd86fee2825731f38badd279
Mason <maseh87@gmail.com>
2014-06-13 17:19:26 -0700

    enemies move every second

--- watchout.js
+++ watchout.js
@@ -8,6 +8,7 @@ var gameSettings = {
 }
 
 //make the Enemies
+//an array of 30 enemy objects with 3 properties. id, x and y
 var makeEnemies = function () {
   return _.range(0, gameSettings.nEnemies).map(function(i) {
     return {
@@ -22,8 +23,32 @@ var enemy = makeEnemies();
 
 
 //make the board
-var board = d3.select('body').append('svg').attr({width: gameSettings.width, height: gameSettings.height, class: "gameBoard"});
+var board = d3.select('body')
+.append('svg')
+.attr({width: gameSettings.width, height: gameSettings.height, class: "gameBoard"});
 
 
 //renderEnemies on the board
-var renderEnemies = board.selectAll('circle').data(enemy).enter().append('circle').attr('cx', function(i){ return i.x; }).attr('cy', function(i){ return i.y; }).attr('id', function(i){ return i.id; }).attr('r', 10).attr('fill', 'blue' );
+//this is an object
+var renderEnemies =
+  board.selectAll('circle')
+  .data(enemy)
+  .enter()
+  .append('circle')
+  .attr('cx', function(i){ return i.x; }).attr('cy', function(i){ return i.y; }).attr('id', function(i){ return i.id; }).attr('r', 10).attr('fill', 'blue' );
+
+//make enemies move every second
+setInterval(function() {
+  renderEnemies.transition().duration(500).attr('cy', function(i){ return Math.random() * 800; }).attr('cx', function(i){ return Math.random() * 600; });
+}, 1000);
+
+
+
+
+
+
+
+
+
+
+
